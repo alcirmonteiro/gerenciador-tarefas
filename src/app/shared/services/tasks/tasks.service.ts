22a45cf0c921769e8b7ea220bb384c4a6d3be12d
Alcir Monteiro <alcirmonteiro@gmail.com>
2025-10-30 20:44:34 -0300

    feat(tasks.service): cria serviço

---
+++ src/app/shared/services/tasks/tasks.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { ITask } from '../../interfaces/task.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+
+  getAll(): Observable<ITask[]> {
+    return of([
+      {title: 'Item 1', completed: false},
+      {title: 'Item 2', completed: false},
+      {title: 'Item 3', completed: false},
+      {title: 'Item 4', completed: true},
+      {title: 'Item 5', completed: true},
+      {title: 'Item 6', completed: true},
+   ]);
+  }
+}
