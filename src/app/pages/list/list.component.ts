9e057ef2934b18d8051f4adf7162776640be9bca
Alcir Monteiro <alcirmonteiro@gmail.com>
2025-10-31 06:46:14 -0300

    test(TaskService): aplica a interface ITask

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ITask } from 'src/app/shared/interfaces/task.interface';
+import { TasksService } from 'src/app/shared/services/tasks/tasks.service';
 
 @Component({
   selector: 'app-list',
@@ -9,12 +10,21 @@ import { ITask } from 'src/app/shared/interfaces/task.interface';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  
+  constructor(
+    private readonly taskService: TasksService
+  ){};
+
+  ngOnInit(): void {
+    this.taskService.getAll().subscribe(tasks => this.tasks.set(tasks));
+  }
 
   tasks = signal<ITask[]>([]);
 
   completedTasks = computed(() => this.tasks().filter(task => task.completed));
   pandingTasks = computed(() => this.tasks().filter(task => !task.completed));
+
 
 
 
